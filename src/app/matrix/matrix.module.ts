4ff13ab865f662d4134ad81d15e9fd83eb38af6d
smithun85 <tech.smithun@gmail.com>
2023-07-18 07:51:19 +0530

    changed

--- src/app/matrix/matrix.module.ts
+++ src/app/matrix/matrix.module.ts
@@ -1,22 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import { MatricesComponent } from './matrices.component';
-import { MatrixRoutingModule } from './matrix-routing';
-import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-@NgModule({
-  declarations: [
-    MatricesComponent
-  ],
-  imports: [
-    CommonModule,
-    RouterModule,
-    ReactiveFormsModule,
-    FormsModule,
-    MatrixRoutingModule,
-=======
 import { MatricesMultiplicationComponent } from './matrices-multiplication/matrices-multiplication.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatrixRoutingModule } from './matrix-routing';
@@ -32,8 +15,6 @@ import { MatrixRoutingModule } from './matrix-routing';
     ReactiveFormsModule,
     FormsModule,
     MatrixRoutingModule
-
->>>>>>> a88f14b26800f56e1afc6006159efcb9a4c7461d
   ]
 })
 export class MatrixModule { }
